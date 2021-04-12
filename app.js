312255523532f05013967464049cba6e4f01a915
Jake Lamarre <jakelamarre@hotmail.com>
2021-04-12 18:30:20 -0400

    node 9.2.5

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
   
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-      console.log(profileItem)
-    });
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
   };
+console.log(name, github);
+console.log(generatePage(name, github));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
   
-  printProfileData(profileDataArgs);
+//     console.log('================');
+  
+//     // Is the same as this...
+//     profileDataArr.forEach((profileItem) => {
+//       console.log(profileItem)
+//     });
+//   };
+  
+//   printProfileData(profileDataArgs);
