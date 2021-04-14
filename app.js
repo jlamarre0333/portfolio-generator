a6c23dbcf8e6be390e579863a1378768bc4b0536
Jake Lamarre <jakelamarre@hotmail.com>
2021-04-14 15:41:27 -0400

    organized code 9.3.1

--- app.js
+++ app.js
@@ -1,53 +1,18 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
 console.log(profileDataArgs);
+
 const [name, github] = profileDataArgs;
 
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-  };
 console.log(name, github);
-console.log(generatePage(name, github));
 
+const pageHTML = generatePage(name, github);
 
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
-
-
-
-
-
-
-
-
-
-
-// // Notice the lack of parentheses around the `profileDataArr` parameter?
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//       console.log(profileDataArr[i]);
-//     }
-  
-//     console.log('================');
-  
-//     // Is the same as this...
-//     profileDataArr.forEach((profileItem) => {
-//       console.log(profileItem)
-//     });
-//   };
-  
-//   printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
